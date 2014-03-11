a6eb3bc169e2f0d97da2c142cb98b4dc6955e675
Bill Pairaktaridis <v.pairak@gmail.com>
2014-03-11 15:35:39 +0200

    Fixed case of values being None.

--- microdev/models.py
+++ microdev/models.py
@@ -14,7 +14,7 @@ from django.utils.encoding import smart_str
 
 
 class ChangeLogManager(models.Manager):
-	
+
 	def log_change(self, user, model, obj_id, field_name, original_value, updated_value, content_type):
 		if len(smart_str(original_value)) > ChangeLog.VALUE_MAX_LENGTH:
 			original_value = smart_str(original_value)[:(ChangeLog.VALUE_MAX_LENGTH-3)] + '...'
@@ -24,34 +24,34 @@ class ChangeLogManager(models.Manager):
 
 		# Create the new log entry
 		self.get_query_set().create(
-									user           =user, 
-									model          =model, 
-									obj_id         =obj_id, 
-									field_name     =field_name, 
-									original_value =smart_str(original_value), 
-									updated_value  =smart_str(updated_value), 
+									user           =user,
+									model          =model,
+									obj_id         =obj_id,
+									field_name     =field_name,
+									original_value =smart_str(original_value),
+									updated_value  =smart_str(updated_value),
 									object_content_type   = content_type)
 
 		# Better readability of long strings
 """--------------------------------------------------------------------
-	Abstract 
+	Abstract
 --------------------------------------------------------------------"""
 class ChangeLog(models.Model):
 	VALUE_MAX_LENGTH = 1024
-	
+
 
 	object_content_type = models.ForeignKey(ContentType, related_name="%(app_label)s_%(class)s_object_content_type", null=True)
 	obj_id              = models.IntegerField()
 	object_instance     = generic.GenericForeignKey('object_content_type', 'obj_id')
 	# By combining 'object_content_type' and 'obj_id' we are able to get the actual model instance for which we are saving the ChangeLog
-	
+
 	date_created        = CreationDateTimeField()
 	user                = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
 	model               = models.CharField(max_length=128)
 	field_name          = models.CharField(max_length=128)
 	original_value      = models.CharField(max_length=VALUE_MAX_LENGTH)
 	updated_value       = models.CharField(max_length=VALUE_MAX_LENGTH)
-	
+
 	objects = ChangeLogManager()
 
 	class Meta:
@@ -70,40 +70,45 @@ class ChangeLogAdmin(admin.ModelAdmin):
 --------------------------------------------------------------------"""
 class ChangeLoggerMixin():
 	_original_state = {}
-	
+
 	# Override in the implementation class to exclude change tracking on listed fields
 	_change_logger_mixin__ignore_list = ['date_updated']
-	
+
 	# Must override to specify which ChangeLog implementation class to write to
 	_change_logger_mixin__change_log_class = None
-	
+
 
 	# Begin tracking changes to the instance
 	def track_changes(self):
 		self._original_state = dict(self.__dict__)
-	
+
 	# Check model's status - name is extended to avoid namespace conflicts
 	def change_logger_mixin__is_changed(self):
 		if not self._change_logger_mixin__change_log_class:
 			raise Exception('microdev.models.ChangeLoggerMixin: _change_logger_mixin__change_log_class was not defined in the implementation class %s' % self.__class__.__name__)
 			return
-		
+
 		missing = None
 		for key, orig_value in self._original_state.iteritems():
 			if key not in self._change_logger_mixin__ignore_list:
 				new_value = self.__dict__.get(key, missing)
-				if smart_str(orig_value) != smart_str(new_value):
-					return True
+				if orig_value:
+					if smart_str(orig_value) != smart_str(new_value):
+						return True
+				else:
+					if not new_value:
+						return True
+
 		return False
 
-					
+
 	# Log any changes to the instance to the specified change_log_class
 	def log_changes(self, user=None):
 		from django.db import transaction
 		if not self._change_logger_mixin__change_log_class:
 			raise Exception('microdev.models.ChangeLoggerMixin: _change_logger_mixin__change_log_class was not defined in the implementation class %s' % self.__class__.__name__)
 			return
-		
+
 
 
 		missing = None
@@ -114,15 +119,15 @@ class ChangeLoggerMixin():
 					if smart_str(orig_value) != smart_str(new_value):
 						# This allows us to get the object's content type to be able to automatically save and store the GenericForeignKey
 						content_type = ContentType.objects.get_for_model(self)
-						
+
 						self._change_logger_mixin__change_log_class.objects.log_change(user, self.__class__.__name__, self.id, key, orig_value, new_value, content_type)
 
 
 
 class ChangeLoggedModel(models.Model, ChangeLoggerMixin):
-	""" 
+	"""
 		A newer, more convenient approach that automatically logs all changes to
-		existing objects. 
+		existing objects.
 	"""
 	class Meta:
 		abstract = True
@@ -142,7 +147,7 @@ class ChangeLoggedModel(models.Model, ChangeLoggerMixin):
 
 class UuidModel(models.Model):
 	"""
-		A simple abstract base class to automatically generate a standard UUID 
+		A simple abstract base class to automatically generate a standard UUID
 		for each instance of a model.
 	"""
 	uuid = models.CharField(max_length=36, null=True, blank=True)
@@ -168,7 +173,7 @@ class UuidModelAdmin(admin.ModelAdmin):
 
 class ShortUuidModel(models.Model):
 	"""
-		A simple abstract base class to automatically generate a short UUID 
+		A simple abstract base class to automatically generate a short UUID
 		for each instance of a model.
 	"""
 	uuid = models.CharField(max_length=22, null=True, blank=True)
@@ -209,7 +214,7 @@ class TimestampModelAdmin(admin.ModelAdmin):
 class CsrNoteModel(models.Model):
 	"""
 		A simple abstract base class for customer service reps to log
-		their actions 
+		their actions
 	"""
 	note = models.TextField()
 	date_created = CreationDateTimeField()
